a2d1dff165877d9e1025fbef709d1d7f110c4dfd
Godis <guil.dendena@gmail.com>
2023-06-21 13:22:00 -0300

    add bash to lsp

--- lua/godis/lsp.lua
+++ lua/godis/lsp.lua
@@ -27,7 +27,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-local servers = { 'pyright', 'tsserver', 'gopls', 'html'}
+local servers = { 'pyright', 'tsserver', 'gopls', 'html', 'bashls'}
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
         capabilities = capabilities,
